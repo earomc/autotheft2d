a59a5ee8de5454702ac0493e323472430cb50fc7
earomc <moritz.brunne@gmail.com>
2024-09-21 01:30:16 +0200

    separate code to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use macroquad::{prelude::*, telemetry::frame};
-use macroquad_tiled as tiled;
+use autotheft2d::{Character, Direction};
+use macroquad::prelude::*;
 
 const WINDOW_HEIGHT: i32 = 720;
 const WINDOW_WIDTH: i32 = 1280;
@@ -32,64 +32,4 @@ async fn main() {
     }
 }
 
-struct Character {
-    facing: Direction,
-    texture: Texture2D
-}
 
-#[derive(Debug, Clone, Copy)]
-enum Direction {
-    NORTH,
-    EAST,
-    SOUTH,
-    WEST
-}
-impl IntoIterator for Direction {
-    type Item = Direction;
-
-    type IntoIter = DirectionIter;
-
-    fn into_iter(self) -> Self::IntoIter {
-        DirectionIter(self)
-    }
-}
-
-struct DirectionIter(Direction);
-
-impl Iterator for DirectionIter {
-    type Item = Direction;
-    
-    fn next(&mut self) -> Option<Self::Item> {
-        let next = match self.0 {
-            Direction::NORTH => Direction::EAST,
-            Direction::EAST => Direction::SOUTH,
-            Direction::SOUTH => Direction::WEST,
-            Direction::WEST => Direction::NORTH,
-        };
-        self.0 = next;
-        Some(next)
-    }
-}
-
-impl Character {
-    fn new(texture: Texture2D) -> Self {
-        texture.set_filter(FilterMode::Nearest);
-        Character { facing: Direction::SOUTH, texture }
-    }
-    
-    pub fn draw(&self) {
-        let x_texture_offset = match self.facing {
-            Direction::NORTH => 2. * 16.,
-            Direction::EAST => 16.,
-            Direction::SOUTH => 0.,
-            Direction::WEST => 3. * 16.,
-        };
-        draw_texture_ex(
-            &self.texture,
-            screen_width() / 2. - self.texture.width() / 2.,
-            screen_height() / 2. - self.texture.height() / 2.,
-            WHITE,
-            DrawTextureParams { dest_size: Some((64., 64.).into()), source: Some(Rect { x: x_texture_offset, y: 0., w: 16., h: 16. }), ..Default::default() }
-        );
-    }
-}
